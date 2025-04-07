4ddd661f4dd91ea2ec7c7ac85b027df170f0de5c
Nicholas Russotti <test@test.com>
2025-04-07 18:33:06 -0400

    Backend Start

---
+++ Backend/models/mentee.model.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const MenteeSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  userType: {
+    enum: ["Mentor", "Mentee"],
+    default: "Mentee",
+  },
+
+// Mentee specific fields:
+requestedMentors:{
+    
+},
+approvedMentors:{
+
+},
+interests: {
+    type: String,
+    required: true,
+},
+
+
+});
+
+module.exports = mongoose.model("Mentee", MenteeSchema);
