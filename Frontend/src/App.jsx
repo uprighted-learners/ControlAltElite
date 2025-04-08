cba16231835ebddd7598ef986b56b86e3b70c824
Nicholas Russotti <test@test.com>
2025-04-07 20:06:23 -0400

    backend-monday

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="grid">
+        <h1 className="text-7xl uppercase text-center">Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
