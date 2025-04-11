347c25f9b432ebdc4223eaeae14596363d27ec8e
vcabwavo <122946682+vcabwavo@users.noreply.github.com>
2025-04-11 12:53:22 -0400

    Endpoints for front end

---
+++ Frontend/src/constants/endpoints.js
@@ -0,0 +1,7 @@
+export const API = "http://localhost:4000";
+
+export const API_LOGIN = `${API}/user/login`;
+
+export const API_REGISTER = `${API}/user/register`;
+
+export const API_MENTOR_PROFILE = `${API}/user/mentor/profile/:id`;
