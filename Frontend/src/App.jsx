207294090c38d27987c1e43a0202f7320c3ea5a9
Ceporah Wiggins <ceporah2@gmail.com>
2025-04-17 11:39:37 -0400

    admin create mentor works

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './App.css'
+import CreateMentor from "./components/Admin/Create-Mentor/CreateMentor";
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <div className="grid">
         <h1 className="text-7xl uppercase text-center">Hello World</h1>
+        <CreateMentor/>
       </div>
     </>
   )
