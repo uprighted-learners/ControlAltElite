fde8a1009a0f91d3387d15c43ced7bd1f1da81fc
John <johnkesterson@johns-MacBook-Pro.local>
2025-04-23 18:27:59 -0400

    John3 Branch

--- Frontend/src/components/Mentors/MentorDashboard.jsx
+++ Frontend/src/components/Mentors/MentorDashboard.jsx
@@ -1,10 +1,42 @@
 //  Mentor Dashboard
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import MenteePreview from "./MenteePreview";
 
 const MentorDashboard = () => {
-  return (
-    <div>MentorDashboard</div>
-  )
-}
+  
+  const [isOpen, setIsOpen] = useState(false);
 
-export default MentorDashboard
+  const [hasPengingMatch, setHasPendingMatch] = useState(false)
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    
+     
+      <div className="p-4">
+      {/* Header Container with Flex */}
+      <div className="flex items-center justify-between">
+      <h1 className="text-xl font-bold"> Mentor Name:</h1>
+      <div>
+        <div tabIndex={0} role="button" className="btn m-1" onClick={toggleDropdown}>
+          Click ⬇️
+        </div>
+        <ul
+          tabIndex={0}
+          className= {isOpen ? "block" : "hidden"}
+        >
+          <li>
+            <a>Item 1</a>
+          </li>
+          <li>
+            <a>Item 2</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  );
+};
+
+export default MentorDashboard;
