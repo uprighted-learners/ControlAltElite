ef820fd3450ff27180a41e14b61f6ba0a0af0ee4
Nicholas Russotti <test@test.com>
2025-05-03 21:24:16 -0400

    backend interest updates

---
+++ Backend/middleware/validate-mentee.js
@@ -0,0 +1,29 @@
+// Used to make it so only mentees can view mentor specific routes
+const Mentee = require("../models/mentee.model");
+
+const validateMentee = async (req, res, next) => {
+  try {
+    if (!req.user) {
+      return res.status(401).json({ message: "You must be logged in as a mentee" });
+    }
+    
+    // Ensure userType is Mentee
+    if (req.userType !== "Mentee") {
+      return res.status(403).json({ message: "You must have mentee privileges to access this" });
+    }
+    
+    // Verify that the user exists as a mentee
+    const isMentee = await Mentee.findById(req.user._id);
+
+    if (!isMentee) {
+      return res.status(404).json({ message: "Mentee user not found" });
+    }
+
+    console.log("Mentee user validation successful");
+    return next();
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = validateMentee;
